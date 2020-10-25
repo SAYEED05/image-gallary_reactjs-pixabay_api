e2571b98d47d9f80b1a6fdf2cc791e2ddc4cf95f
SAYEED <sayeed.705@gmail.com>
2020-10-25 15:01:26 +0530

    fixed minor bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import ImageCard from './components/imageCard';
 import ImageSearch from './components/imagesearch';
-
+import './assets/customStyle.css'
+import logo from './assets/img/pixabay.svg'
 
 function App() {
   const [images, setImages] = useState([]);
@@ -9,7 +10,7 @@ function App() {
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true&per_page=200`)
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true&per_page=12`)
       .then(res => res.json())
       .then(data => {
         setImages(data.hits);
@@ -19,6 +20,7 @@ function App() {
   }, [term]);
   return (
     <div className="container mx-auto">
+      <div className='toptxt'><h4>Created Using  <a href='https://pixabay.com/'><img className='logo' src={logo} /></a> API</h4></div>
       <ImageSearch searchText={(text) => setTerm(text)} />
 
       {!isLoading && images.length === 0 && <h1 className='text-5xl text-center mx-auto mt-32'>No Images Found For The Term '{term}'</h1>}
@@ -28,7 +30,7 @@ function App() {
 
         :
 
-        <div class="flex flex-wrap -mx-1 lg:-mx-4 gap-4">
+        <div className="flex flex-wrap -mx-1 lg:-mx-4 gap-4">
 
           {
             images.map(image => (
@@ -38,6 +40,7 @@ function App() {
 
 
         </div>}
+
     </div>
   );
 }
--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+
 const ImageCard = ({ image }) => {
     const tags = image.tags.split(',');
     const user = 'https://pixabay.com/users/' + image.user + '-' + image.user_id
+    /*     const tagclick = 'https://pixabay.com/images/search/' + tags */
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="wid rounded overflow-hidden shadow-lg">
             <a href={image.largeImageURL} > <img src={image.webformatURL} alt="" className="w-full" /></a>
             <div className="px-6 py-4">
                 <div className="font-bold  text-black-500 text-xl mb-2">
-                    PHOTO BY <a target="_blank" href={user}><span className='italic text-teal-400'>{image.user}</span></a>
+                    PHOTO BY <a target="_blank" href={user}><span className='italic userlink text-teal-400'>{image.user}</span></a>
                 </div>
                 <ul>
                     <li>
@@ -26,7 +28,7 @@ const ImageCard = ({ image }) => {
                 </ul>
             </div>
             <div className="px-6 pt-4 pb-2">
-                {tags.map((tag, index) => (<span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#{tag}</span>))}
+                {tags.map((tag, index) => (<span key={index} className="tagstyle inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#{tag}</span>))}
 
             </div>
         </div>
