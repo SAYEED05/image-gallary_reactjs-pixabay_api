39678da6bf508d7cb91d4ecfcd8ae58e2f5f0fa4
SAYEED <sayeed.705@gmail.com>
2020-10-25 15:32:04 +0530

    Updates

--- precache-manifest.3a06bdd7a7aec047654ee38007261630.js
+++ precache-manifest.3a06bdd7a7aec047654ee38007261630.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41ee6b485803ff8f99a197bedef5ac12",
+    "revision": "4c15b17b7f0e447a1723c3bcf8d00de9",
     "url": "/image-gallary_reactjs-pixabay_api/index.html"
   },
   {
-    "revision": "2e7feca4725e222a730f",
+    "revision": "087bc233c1ca2621d037",
     "url": "/image-gallary_reactjs-pixabay_api/static/css/main.827a3f52.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/image-gallary_reactjs-pixabay_api/static/js/2.61463324.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2e7feca4725e222a730f",
-    "url": "/image-gallary_reactjs-pixabay_api/static/js/main.6d45c648.chunk.js"
+    "revision": "087bc233c1ca2621d037",
+    "url": "/image-gallary_reactjs-pixabay_api/static/js/main.9ff245c4.chunk.js"
   },
   {
     "revision": "b8ba9ba3fcf01d099c90",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/image-gallary_reactjs-pixabay_api/precache-manifest.d9afe7b402effe2c31adf6878f656a6d.js"
+  "/image-gallary_reactjs-pixabay_api/precache-manifest.3a06bdd7a7aec047654ee38007261630.js"
 );
 
 self.addEventListener('message', (event) => {
